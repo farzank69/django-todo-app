0902a65d751d0d872dc38280a4f137ee87e584de
farzank69 <91688069+farzank69@users.noreply.github.com>
2025-10-26 20:55:22 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todo',
-    'widget_tweaks',
     "django.contrib.sites",     # it requires for allauth
     'allauth',
     'allauth.account',
